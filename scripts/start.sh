d9578432ffb5f70633b633157f8589c1e50ed5f3
Arpit Tripathi (a04t) <arpit04tripathi@gmail.com>
2023-09-25 18:17:18 +0100

    rename to compose yml

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,1 @@
-docker compose -f ./docker/start.yml up
+docker compose -f ./docker/compose.yml up
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,1 +1,1 @@
-docker compose -f ./docker/start.yml down
+docker compose -f ./docker/compose.yml down
