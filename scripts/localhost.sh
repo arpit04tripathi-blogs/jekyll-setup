921c7a70c28a95d7d0db6f37a2e56d6331b2d444
Arpit Tripathi (a04t) <arpit04tripathi@gmail.com>
2023-10-10 16:23:53 +0100

    update scripts

--- scripts/localhost.sh
+++ scripts/localhost.sh
@@ -7,17 +7,17 @@ echo =========================================================
 case $1 in
   --new)
     echo "Creating new project"
-    docker compose -f ./docker/create-site.yml up
+    docker compose -f ./docker/compose.yml up create-new
     mv new_site/* .
     rm -r new_site
     ;;
   --stop)
     echo "running docker compose down"
-    docker compose -f ./docker/compose.yml down
+    docker compose -f ./docker/compose.yml down server
     ;;
   *)
     echo docker compose up
-    docker compose -f ./docker/compose.yml up -d
+    docker compose -f ./docker/compose.yml up server -d
     cmd=$( open -a "Google Chrome" http://localhost:9999/jekyll-setup/ )
     echo opening chrome $cmd
     ;;
