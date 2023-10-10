46336525368cfb0a3e1a30b1c8563101abf63046
Arpit Tripathi (a04t) <arpit04tripathi@gmail.com>
2023-10-10 16:32:05 +0100

    rename docker-compose yml

--- scripts/localhost.sh
+++ scripts/localhost.sh
@@ -7,17 +7,17 @@ echo =========================================================
 case $1 in
   --new)
     echo "Creating new project"
-    docker compose -f ./docker/compose.yml up create-new
+    docker compose -f ./docker/docker-compose.yml up create-new
     mv new_site/* .
     rm -r new_site
     ;;
   --stop)
     echo "running docker compose down"
-    docker compose -f ./docker/compose.yml down server
+    docker compose -f ./docker/docker-compose.yml down server
     ;;
   *)
     echo docker compose up
-    docker compose -f ./docker/compose.yml up server -d
+    docker compose -f ./docker/docker-compose.yml up server -d
     cmd=$( open -a "Google Chrome" http://localhost:9999/jekyll-setup/ )
     echo opening chrome $cmd
     ;;
